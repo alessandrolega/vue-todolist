71a5863f0647f10adf1326907502cd7d762da1ff
alessandrolega <alessandrolega24@gmail.com>
2022-11-14 23:37:19 +0100

    creato array di oggetti e aggiunta funzione click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+      todoList : [
+        {
+            text: 'Comprare il pane',
+            done : false
+        },
+
+        {
+            text: 'Comprare il pesce',
+            done : false
+        }
+      ]
+    },
+    methods: {
+        todoComplete(index){
+            this.todoList[index].done = true;
+
+        }
     }
   })
